e0f8aa01d0c523d626efbdc2b2cb02fbcf2793c2
Aloukik Patel <41272007+Aloukik122@users.noreply.github.com>
2026-06-28 22:29:58 +0530

    new logic
    i changed the whole thing with new logic because language was not changing in my firefox browser

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let voiceSelect = document.getElementById('voiceSelect');
+
+//this is for the firefox/safari and other browser
+function populateVoice() {
+  voices = window.speechSynthesis.getVoices();
+
+  speech.voice = voices[0];
+
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i);
+  });
+}
+populateVoice();
+
+//this is for the crome/eage/opera browser
+window.speechSynthesis.onvoiceschanged = () => {
+  populateVoice();
+};
+voiceSelect.addEventListener('change', () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+// Triger speech when it clicks
 document.querySelector('button').addEventListener('click', () => {
   speech.text = document.querySelector('textarea').value;
   window.speechSynthesis.speak(speech);
 });
-
-let voiceSelect = document.getElementById('voiceSelect');
-
-function voiceList() {
-  voiceSelect.innerHTML = '<option value="">Select a voice</option>';
-
-  const voices = speechSynthesis.getVoices();
-  const englishVoices = voices.filter((voice) => voice.lang.startsWith('en')); // use to show limited voices
-
-  for (const voice of englishVoices) {
-    const option = document.createElement('option');
-    option.textContent = `${voice.name} (${voice.lang})`;
-    option.value = voice.name; // Stores the voice name to look it up later
-    voiceSelect.appendChild(option); // Appends the option to the dropdown
-  }
-}
-voiceList();
